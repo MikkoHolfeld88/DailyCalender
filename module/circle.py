dfc932b656c7bd1592f3b4e947ef25373d87831b
MikkoTheOne <mikko.holfeld@gmx.de>
2019-09-30 12:19:13 +0200

    Implementing Circle

--- module/circle.py
+++ module/circle.py
@@ -2,9 +2,16 @@ from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.lang import Builder
 
+Builder.load_file("kv\\circle.kv")
+
+class Circle(Widget):
+    def build(this):
+        pass
+
 class CircleApp(App):
 
     def build(self):
-        return Builder.load_file("kv\\circle.kv")
-
-CircleApp().run()
+        return Circle()
+    
+if __name__ == "__main__":
+    CircleApp().run()
